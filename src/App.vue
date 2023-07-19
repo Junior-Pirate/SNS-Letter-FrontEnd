7298620b7bba6c01b1b45974e5c00a591d60a95c
Kor-NOH <shehddnjs1982@naver.com>
2023-07-19 14:59:08 +0900

    checking filled form

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <div>
       <input type="text" v-model="name" placeholder="Enter your name">
+      <span v-if="!nameFilled" style="color: red;">Please enter your name</span>
     </div>
     <div>
       <input type="text" v-model="email" placeholder="Enter your email">
+      <span v-if="!emailFilled" style="color: red;">Please enter your email</span>
     </div>
     <div>
       <input type="text" v-model="pw" placeholder="Enter your pw">
+      <span v-if="!pwFilled" style="color: red;">Please enter your pw</span>
     </div>
     <div>
       <input type="text" v-model="again_pw" placeholder="Check your pw">
+      <span v-if="!again_pwFilled" style="color: red;">Please enter your again_pw</span>
     </div>
     <button @click="submit">Submit</button>
   </div>
@@ -34,9 +38,13 @@ export default {
   data() {
     return {
       name: '',
+      nameFilled: true,
       email: '',
+      emailFilled: true,
       pw: '',
+      pwFilled: true,
       again_pw: '',
+      again_pwFilled: true,
       items: [], // 빈 배열로 초기화하여 받은 아이템을 저장합니다.
     };
   },
@@ -54,6 +62,29 @@ export default {
         pw: this.pw,
         again_pw: this.again_pw,
       };
+      // 입력란이 채워져있는지 확인.
+      if (this.name.trim() === '') {
+        this.nameFilled = false;
+      } else {
+        this.nameFilled = true;
+        console.log('Name:', this.name);
+      }
+      if (this.email.trim() === '') {
+        this.emailFilled = false;
+      } else {
+        this.emailFilled = true;
+        console.log('Email:', this.email);
+      }
+      if (this.pw.trim() === '') {
+        this.pwFilled = false;
+      } else {
+        this.pwFilled = true;
+      }
+      if (this.again_pw.trim() === '') {
+        this.again_pwFilled = false;
+      } else {
+        this.again_pwFilled = true;
+      }
 
       try {
         // POST 요청을 보냅니다.
