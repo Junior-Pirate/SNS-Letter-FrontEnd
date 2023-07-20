f5745319da549163fb1284b1686a1092d418c2cd
Kor-NOH <shehddnjs1982@naver.com>
2023-07-20 14:00:13 +0900

    add login.vue

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 import Register from "@/views/Register-component.vue";
+import Login from "@/views/Login-component.vue";
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: "/register",
         name: "Register",
         component: Register,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
     },
 ];
 
