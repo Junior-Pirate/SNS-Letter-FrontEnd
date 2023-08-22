a6f2561512302afcf770f76e10e17f68d95426f5
Kor-NOH <shehddnjs1982@naver.com>
2023-08-22 18:12:27 +0900

    edit authority page

--- src/views/letterbox-component.vue
+++ src/views/letterbox-component.vue
@@ -16,104 +16,28 @@
 import axios from 'axios';
 
 export default {
-  data() {
-    return {
-      email: '',
-      emailFilled: true,
-      pw: '',
-      pwFilled: true,
-      items: [], // 빈 배열로 초기화하여 받은 아이템을 저장합니다.
-    };
-  },
-  created() {
-    // 컴포넌트가 생성될 때 데이터를 가져옵니다.
-    this.fetchItems();
-  },
-  methods: {
-    //POST 요청
-    async submit() {
-      // 요청 페이로드를 준비합니다.
-      const payload = {
-        email: this.email,
-        pw: this.pw,
-      };
-      // 입력란이 채워져있는지 확인.
-      if (this.email.trim() === '') {
-        this.emailFilled = false;
-      } else {
-        this.emailFilled = true;
-        console.log('Email:', this.email);
+  mounted() {
+    axios.post('http://localhost:9000/check', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
       }
-      if (this.pw.trim() === '') {
-        this.pwFilled = false;
-      } else {
-        this.pwFilled = true;
-      }
+    })
+        .then(response => {
+          // 보호된 데이터 처리
+          console.log('success')
+          console.log(response.data);
+        })
+        .catch(error => {
+          // 인증 오류 처리
+          console.log('fail')
+          console.error(error);
+        });
+  }
+}
 
-      try {
-        // POST 요청을 보냅니다.
-        await axios.post('http://localhost:9000/login', payload);
-
-        // 응답 데이터를 처리합니다.
-        this.fetchItems();
-
-      } catch (error) {
-        // 오류 처리
-        console.error(error);
-      }
-    },
-    //GET 요청
-    async fetchItems() {
-      try {
-        // GET 요청을 보냅니다.
-        const response = await axios.get('http://localhost:9000/login');
-
-        // 응답 데이터를 받아서 items에 저장합니다.
-        this.items = response.data;
-      } catch (error) {
-        // 오류 처리
-        console.error(error);
-      }
-    },
-    movetohome() {
-      window.location.href = '/home'
-    },
-  },
-};
 </script>
 
 
 <style>
-
-#login-background {
-  background: #6554AF;
-  width: 500px;
-  height: 110px;
-  border-radius: 50px;
-  display: flex;
-  margin: 0 auto;
-  clear: both;
-}
-
-#login-form1 {
-  background: gray;
-  width: 170px;
-  height: 80%;
-  border-radius: 50px;
-  float: left;
-  top: 10px;
-  left: 20px;
-  position: relative;
-}
-
-#login-form2 {
-  width: 280px;
-  height: 50%;
-  border-radius: 50px;
-  float: right;
-  top: 13px;
-  left: 30px;
-  position: relative;
-}
 
 </style>
