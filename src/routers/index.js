9656cbad9321bf1bb40adfe6dd6251a34c66a96d
Kor-NOH <shehddnjs1982@naver.com>
2023-12-03 22:53:20 +0900

    when enter letterbox, check userid and useremail

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,4 @@
 import { createWebHistory, createRouter } from "vue-router";
-// import HelloWorld from "@/components/HelloWorld.vue";
-// import Home from "@/views/Home-component.vue";
 import Register from "@/views/Register-component.vue";
 import Login from "@/views/Login-component.vue";
 import Letterbox from "@/views/letterbox-component.vue"
@@ -8,14 +6,8 @@ import Writeletter from "@/views/Writeletter-component.vue"
 import Exit from "@/views/Exit-component.vue";
 import Maintest from "@/views/Main-component.vue"
 import Error from "@/views/404error-component.vue"
-import Test from "@/views/Test-component.vue"
 
 const routes = [
-    // {
-    //     path: "/home",
-    //     name: "Home",
-    //     component: Home,
-    // },
     {
         path: "/main",
         name: "Maintest",
@@ -51,11 +43,6 @@ const routes = [
         path: "/:catchAll(.*)",
         name: "404",
         component: Error,
-    },
-    {
-        path: "/user/:userID",
-        name: "letterbox",
-        component: Test,
     }
 ];
 
--- src/views/letterbox-component.vue
+++ src/views/letterbox-component.vue
@@ -4,12 +4,23 @@
   <div id="div-right">
     <button id="button" @click="movetohome">홈</button>
   </div>
-  <div id="login-background">
-    <div id="login-form2">
-      <a> 이 곳은 개인 편지함 </a><br>
 
+  <div v-if="showInput">
+    <input v-model="userEmail" placeholder="이메일 입력">
+    <button @click="checkUser">확인</button>
+  </div>
+  <!--showContent가 true이면 본문이 보이도록
+  접근한 userid와 입력한 useremail이 같은 사람이라면
+  showcontent를 true로 변경-->
+  <div v-if="showContent">
+
+    <div id="login-background">
+      <div id="login-form2">
+        <a> 이 곳은 개인 편지함 </a><br>
+      </div>
     </div>
   </div>
+
   </body>
 </template>
 
@@ -17,11 +28,23 @@
 <script>
 export default {
   props: ['userId'],
+  data() {
+    return {
+      userEmail: '',
+      showContent: false,
+      showInput: true,
+    };
+  },
   methods: {
     // 페이지 이동시 params로 게시판 구분, query로 페이지 구분
     movetohome() {
-      window.location.href = '/home'
+      window.location.href = '/main'
     },
+    checkUser() {
+      // 백엔드로 useremail 넘기고 확인하는 로직 추가
+      this.showContent = true; // 본문은 보이도록 true
+      this.showInput = false; // email 입력란은 안보이도록 false
+    }
   },
 };
 </script>
